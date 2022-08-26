e41a08793ca51d0fbe9efebe9a289a4975584a4a
pab-lo-hub <salaberry.pablo@gmail.com>
2022-08-26 18:53:13 +0200

    Formd added and bootstrap details

--- src/pages/table.js
+++ src/pages/table.js
@@ -14,6 +14,16 @@ export default function Table(props) {
     return (
         <div>
             <h1>TABLE</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th width="25%">Nombre</th>
+                        <th width="25%">Edad</th>
+                        <th width="25%">Carrera</th>
+                        <th width="25%">Hobbie</th>
+                    </tr>
+                </thead>
+            </table>
             {items}
         </div >
     )
